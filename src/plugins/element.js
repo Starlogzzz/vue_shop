f59e084b528298a51136d77e5fcb5b09d3e69273
Starlog <785276471@qq.com>
2020-08-05 09:16:26 +0800

    完成分类参数开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog, MessageBox, Tag, Tree, Select, Option, Cascader } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, 
+    Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem,
+    Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination,
+    Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert,
+    Tabs, TabPane,Steps,Step,CheckboxGroup,Checkbox } from 'element-ui'
 
 
 Vue.use(Button)
@@ -29,5 +33,12 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(Tabs)
+Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
