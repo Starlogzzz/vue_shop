7850df3e703dd797375d5bd2cf613b2158926601
Starlog <785276471@qq.com>
2020-08-08 14:39:58 +0800

    完成订单功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,7 +5,8 @@ import { Button, Form, FormItem, Input, Message, Container, Header,
     Dialog, MessageBox, Tag, Tree, Select, Option, Cascader, Alert,
     Tabs, TabPane,Steps,Step,CheckboxGroup,Checkbox, Upload } from 'element-ui'
 
-
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -41,6 +42,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
